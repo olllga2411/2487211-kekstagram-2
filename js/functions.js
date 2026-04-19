a6bc842a60d67b80409820b8150c714111055fb1
olllga2411 <mblxova@mail.ru>
2026-04-19 17:01:26 +0300

    написана первая функция

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+// Функция для проверки длины строки. Она принимает строку, которую нужно проверить на максимальную длину, и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее. Эта функция нам пригодится для валидации формы.
+
+const comparisonLengthString = function (string, maxLenth) {
+  const factLength = string.length;
+  if (factLength <= maxLenth) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+comparisonLengthString ('кольцо', 20);
+comparisonLengthString ('привет из Питера', 25);
+comparisonLengthString ('кукушка', 2);
+
+// Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
