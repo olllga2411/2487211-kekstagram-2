c732ab49f2c713a471f38691537f0c62bb7f82c8
olllga2411 <mblxova@mail.ru>
2026-04-21 02:21:26 +0300

    написана треться функция

--- js/functions.js
+++ js/functions.js
@@ -34,3 +34,55 @@ palindrome('я люблю');
 palindrome('ДовОд');
 palindrome('Лёша на полке клопа нашёл');
 
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN ПЕРВЫЙ ВАРИАНТ
+
+const findNumber1 = function (string) {
+  string = string.toString();
+  let number = '';
+  for (let i = 0; i <= string.length; i++) {
+    if (parseInt(string[i], 10) === parseInt(string[i], 10)) {
+      number = number + parseInt(string[i], 10);
+    }
+  }
+  if (number === '') {
+    return NaN;
+  }
+  console.log(number);
+};
+
+findNumber1('2023 год');
+findNumber1('ECMAScript 2022');
+findNumber1('1 кефир, 0.5 батона');
+findNumber1('агент 007');
+findNumber1('а я томат');
+findNumber1(2023);
+findNumber1(-1);
+findNumber1(1.5);
+
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN ВТОРОЙ ВАРИАНТ
+
+const findNumber2 = function (string) {
+  string = string.toString();
+  let number = '';
+  for (let i = 0; i <= string.length; i++) {
+    const getNumber = parseInt(string[i], 10);
+    if (Number.isNaN(getNumber)){
+    } else {
+      number = number + getNumber;
+    }
+  }
+  if (number === '') {
+    return NaN;
+  }
+  console.log(number);
+};
+
+
+findNumber2('2023 год');
+findNumber2('ECMAScript 2022');
+findNumber2('1 кефир, 0.5 батона');
+findNumber2('агент 007');
+findNumber2('а я томат');
+findNumber(2023);
+findNumber2(-1);
+findNumber2(1.5);
