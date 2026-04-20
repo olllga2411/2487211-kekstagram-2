eee07f35dde314e9490241773538d2db5af95037
olllga2411 <mblxova@mail.ru>
2026-04-21 00:27:50 +0300

    написана вторая функция

--- js/functions.js
+++ js/functions.js
@@ -14,3 +14,23 @@ comparisonLengthString ('привет из Питера', 25);
 comparisonLengthString ('кукушка', 2);
 
 // Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
+
+const palindrome = function (string) {
+  let processingString = string.replaceAll(' ','');
+  processingString = processingString.toLowerCase();
+  const lengthString = processingString.length;
+  let reverseString = '';
+  for (let i = lengthString - 1; i > -1; i = i - 1) {
+    reverseString = reverseString + processingString[i];
+  }
+  if (processingString === reverseString) {
+    console.log (`${string } - является палидромом`);
+  } else {
+    console.log (`${string } - не является палидромом`);
+  }
+};
+
+palindrome('я люблю');
+palindrome('ДовОд');
+palindrome('Лёша на полке клопа нашёл');
+
