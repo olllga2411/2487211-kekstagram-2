cb6fdc16aa30930cc78398342da2b35a9632dbd4
olllga2411 <mblxova@mail.ru>
2026-06-06 17:14:24 +0300

    подправила вторую ф-цию

--- js/functions.js
+++ js/functions.js
@@ -19,19 +19,25 @@ comparisonLengthString ('кукушка', 2);
 
 // Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
 
-const palindrome = function (string) {
-  let processingString = string.replaceAll(' ','');
-  processingString = processingString.toLowerCase();
-  const lengthString = processingString.length;
-  let reverseString = '';
-  for (let i = lengthString - 1; i > -1; i = i - 1) {
-    reverseString = reverseString + processingString[i];
-  }
-  if (processingString === reverseString) {
-    console.log (`${string } - является палидромом`);
-  } else {
-    console.log (`${string } - не является палидромом`);
-  }
+// const palindrome = function (string) {
+//   let processingString = string.replaceAll(' ','');
+//   processingString = processingString.toLowerCase();
+//   const lengthString = processingString.length;
+//   let reverseString = '';
+//   for (let i = lengthString - 1; i > -1; i = i - 1) {
+//     reverseString = reverseString + processingString[i];
+//   }
+//   if (processingString === reverseString) {
+//     console.log (`${string } - является палидромом`);
+//   } else {
+//     console.log (`${string } - не является палидромом`);
+//   }
+// };
+
+const palindrome = (string) => {
+  const processingString = string.replaceAll(' ','').toLowerCase();
+  const reverseString = processingString.split('').reverse().join('');
+  return (processingString === reverseString);
 };
 
 palindrome('я люблю');
